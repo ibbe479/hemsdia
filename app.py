51e86a2ef1bfec1bc096a737e4c48a755c591e32
ibbe479 <ibrahim.halawani04@gmail.com>
2025-09-21 02:50:18 +0200

    dkfdk

--- app.py
+++ app.py
@@ -6,34 +6,11 @@ app = Flask(__name__)
 
 
 jobb_lista = [
-    Jobb(
-        titel="Lastbilsförare",
-        beskrivning="Transporterar gods över korta eller långa sträckor, antingen nationellt eller internationellt.",
-        lön="23 000 - 30 000kr",
-        plats="Malmö"
-    ),
-    Jobb(
-        titel="Butiksbiträde",
-        beskrivning="Hjälper kunder och sköter kassan i butik.",
-        lön="20 000 - 25 000kr",
-        plats="Stockholm"
-    ),
+    
 ]
 
 beskrvningar=[
-    JobbDetaljer(
-        "Dina huvudsakliga arbetsuppgifter är att planera och utföra dagliga leveranser av pallgods i Malmö med omnejd. Detta inkluderar att ansvara för säker lastning och lossning av varor samt att hantera leveransdokument för att se till att alla uppgifter är korrekta. Dessutom ingår det att utföra daglig tillsyn av fordonet för att säkerställa att det är i gott skick.",
-        "För att vara aktuell för tjänsten krävs det att du har C-körkort och ett giltigt YKB (Yrkeskompetensbevis). Vi ser det som en stor fördel om du har minst två års erfarenhet av att köra lastbil, och truckkort är meriterande. Du bör även ha goda kunskaper i svenska, både i tal och skrift, och vara en ansvarsfull, noggrann och självständig person.",
-        "Vi erbjuder en konkurrenskraftig månadslön på 23 000 – 30 000 kr beroende på din erfarenhet och eventuell obekväm arbetstid. Dessutom får du förmåner som friskvårdsbidrag och betald övertid. Du får arbeta i ett litet, familjärt team med god sammanhållning.", 
-        "Arbetstiden är på heltid, måndag till fredag, med start kl. 07:00 och slut kl. 16:00."
-        ),
     
-    JobbDetaljer(
-        "Som butiksbiträde är dina huvudsakliga arbetsuppgifter att hjälpa kunder, ta betalt vid kassan, fylla på varor och hålla butiken städad. Du kommer att vara företagets ansikte utåt och ska ge en utmärkt service till alla kunder.",
-        "Vi söker dig som är serviceinriktad, har god social kompetens och kan arbeta självständigt. Tidigare erfarenhet av butiksarbete är meriterande men inte ett krav. Du behöver vara över 18 år och ha goda kunskaper i svenska.",
-        "Vi erbjuder en timlön på 110 kr, personalrabatt, och möjligheter till vidareutbildning och avancemang inom företaget. Du kommer att arbeta i ett trevligt team med härliga kollegor.", 
-        "Arbetstiderna varierar mellan dag- och kvällspass, inklusive helger. Arbetstiden är på deltid, med möjlighet till utökning vid behov."
-    )
 ]
 
 
